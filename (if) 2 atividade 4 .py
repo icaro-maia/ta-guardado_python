3ef79d9ea3e3880e08f6b0b396d43c0680ba8866
icaro-maia <icaromaia1612@gmail.com>
2025-06-13 19:06:41 -0300

    Create (if) 2 atividade 4 .py
    if

---
+++ (if) 2 atividade 4 .py
@@ -0,0 +1,18 @@
+chuva = False
+nublado = False
+
+
+if chuva :
+  print " tenho que pegar meu guarda chuva"
+else :
+  if nublado :
+    print " levarei o guarda chuva"
+chuva = False
+nublado = False
+
+
+if chuva :
+  print " tenho que pegar meu guarda chuva"
+else :
+  if nublado :
+    print " levarei o guarda chuva"
