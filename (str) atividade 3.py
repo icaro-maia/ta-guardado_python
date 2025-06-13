c3974020fb224a9e6783fac3f29da1ee2db6d750
icaro-maia <icaromaia1612@gmail.com>
2025-06-13 19:04:48 -0300

    Create (str) atividade 3.py
    str

---
+++ (str) atividade 3.py
@@ -0,0 +1,14 @@
+idade = 14
+minhaidade1 = "minha idade "
+#print minhaidade1 + str(idade)
+
+
+altura = "1.79 "idade += float(altura)
+#print "mas minha altura e " + altura + minhaidade1 + str(int(idade))
+
+
+
+
+altura = True
+idade = False
+print altura + idade
