1a2b5ace8924191541c1c50a624d1faf03f72865
icaro-maia <icaromaia1612@gmail.com>
2025-06-20 20:55:12 -0300

    add input

---
+++ (input) atividade 6.py
@@ -0,0 +1,19 @@
+"""
+#Description:mad lip python
+#Author:  icaro
+"""
+
+# The template for the story
+
+STORY = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world."
+
+print ("Mad Libs foi iniciado.")
+
+name = input("Enter a name: ")
+adjetivo = input("Enter a adjective: ")
+adjetivo2 = input("Enter a adjective: ")
+adjetivo3= input("Enter a adjective: ")
+
+print (name+ adjetivo + adjetivo2 + adjetivo3)
+
+
