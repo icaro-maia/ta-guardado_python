ce9a8d2fd6ffc039a9bd7c4d39c0efd83b640f6b
icaro-maia <icaromaia1612@gmail.com>
2025-08-22 20:13:41 -0300

    Create atividade 11.1.py
    gdvcbuhergbyv

---
+++ atividade 11.1.py
@@ -0,0 +1,17 @@
+import random
+numeroescolhido = 0 
+numerodeaaleatorio = int(input("de qual a qual numero voce quer "))
+numerodechanses = int(input("quantas tentativas voce quer "))
+numerocoreto = random.randint(1,numerodeaaleatorio)
+
+while (numeroescolhido != numerocoreto) and numerodechanses > 0 : 
+    numeroescolhido = int(input("coloque um numero: "))
+    if numeroescolhido == numerocoreto:
+        print("bom trabalho o numero era " + str(numeroescolhido))
+    elif numeroescolhido > numerocoreto:
+       print("tente um numero menor")
+    else:  
+      print("tente um numero maior")
+    numerodechanses -= 1
+    if numerodechanses == 0:
+        print("acabaram suas tentativas e o numero era " + str("numerodeaaleatorio"))
