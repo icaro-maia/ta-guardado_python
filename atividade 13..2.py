5cc0d9eb59e0fddc71c072ace4eb1fe759cefad6
icaro-maia <icaromaia1612@gmail.com>
2025-09-05 20:21:13 -0300

    Create atividade 13..2.py

---
+++ atividade 13..2.py
@@ -0,0 +1,8 @@
+jogos =[ "Hollow Knight " , "Silksong " , "Fifa " , "Temple Run " ,"Dead Cells ", "Castlevania "]
+for game in jogos:
+    if game == "Hollow Knight " :
+            print(game + "e  meu jogo favorito")
+    elif game == "Fifa ":
+          print (game + "obra satanica da humanidade")
+    else:
+          print (game + "bons de mais")
