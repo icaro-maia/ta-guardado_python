98a7fe1a9bdfa9013774f8db262417eacea1446f
icaro-maia <icaromaia1612@gmail.com>
2025-09-05 20:40:30 -0300

    Create atividade 13.3.py

---
+++ atividade 13.3.py
@@ -0,0 +1,17 @@
+palavra = input ("coloque a palavra escolida: ")
+for letra in palavra:
+    print (letra)
+    if letra == "i":
+        print ("uma letra do meu nome serio?")
+    elif letra == "c":
+        print ("uma letra do meu nome serio?")
+    elif letra == "a":
+        print ("uma letra do meu nome serio?")
+    elif letra == "r":
+        print ("uma letra do meu nome serio?")
+    elif letra == "o":
+        print ("uma letra do meu nome serio?")
+if palavra == "icaro":
+    print ("obrigado pela honra mas vai fazer o joginho po")
+
+
